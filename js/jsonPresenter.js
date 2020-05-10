13450b13151305af6c085ae386fcf3cee10fa2c3
Graham Trott <gtanyware@gmail.com>
2020-05-10 11:58:46 +0100

    First running script

--- js/jsonPresenter.js
+++ js/jsonPresenter.js
@@ -1,3 +1,53 @@
+// JSON::Presenter
+
+window.onload = () => {
+    const createCORSRequest = (url) => {
+        var xhr = new XMLHttpRequest();
+        if (`withCredentials` in xhr) {
+    
+            // Check if the XMLHttpRequest object has a "withCredentials" property.
+            // "withCredentials" only exists on XMLHTTPRequest2 objects.
+            xhr.open(`GET`, url, true);
+    
+        } else if (typeof XDomainRequest != `undefined`) {
+    
+            // Otherwise, check if XDomainRequest.
+            // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+            xhr = new XDomainRequest();
+            xhr.open(`GET`, url);
+    
+        } else {
+    
+            // Otherwise, CORS is not supported by the browser.
+            xhr = null;
+    
+        }
+        return xhr;
+    };	
+    
+    const container = document.getElementById(`jp-container`);
+
+    const scriptElement = document.getElementById(`jp-script`);
+    const request = createCORSRequest(scriptElement.innerText);
+    if (!request) {
+        throw Error(`Unable to access the JSON script`);
+    }
+
+    request.onload = () => {
+        if (200 <= request.status && request.status < 400) {
+            JSON_Presenter.present(container, request.responseText);
+        } else {
+            throw Error(`Unable to access the JSON script`);
+        }
+    };
+
+    request.onerror = () => {
+        throw Error(`Unable to access the JSON script`);
+    };
+
+    request.send();
+};
+
 const JSON_Presenter = {
 
     styles: [
@@ -5,14 +55,12 @@ const JSON_Presenter = {
         `background`
     ],
 
-    present: (containerId, text) => {
-        console.log(`This is the presenter, using ${containerId}`);
-
-        JSON_Presenter.container = document.getElementById(containerId);
+    present: (container, text) => {
+        JSON_Presenter.container = container;
 
         const script = JSON.parse(text);
 
-        JSON_Presenter.doGlobal(script.global);
+        JSON_Presenter.doGlobal(script);
 
         let step = 0;
         JSON_Presenter.doStep(script, step);
@@ -24,12 +72,12 @@ const JSON_Presenter = {
         }
     },
 
-    doGlobal: global => {
+    doGlobal: script => {
         const container = JSON_Presenter.container;
-        const height = Math.round(parseFloat(container.offsetWidth) * global.aspectH / global.aspectW);
+        const height = Math.round(parseFloat(container.offsetWidth) * script.aspectH / script.aspectW);
         container.style[`height`] = `${Math.round(height)}px`;
         for (const item of JSON_Presenter.styles) {
-            JSON_Presenter.doStyle(container, global.init, item);
+            JSON_Presenter.doStyle(container, script.default, item);
         } 
         container.style[`background-size`] = `cover`;
     },
@@ -38,11 +86,5 @@ const JSON_Presenter = {
         console.log(script);
         const step = script.steps[stepno];
         console.log(step);
-        const theme = script.global.themes[step.theme];
-        console.log(theme);
     }
 };
-
-window.onload = function () {
-    JSON_Presenter.present(script);
-}
